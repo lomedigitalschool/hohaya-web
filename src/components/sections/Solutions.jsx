e97ea5fbfc6995e4bd9a1ff37373356e3c060901
Gold1705 <mkpascal02@gmail.com>
2026-05-28 20:37:44 +0000

    mise a jour

---
+++ src/components/sections/Solutions.jsx
@@ -0,0 +1,9 @@
+
+function Solutions() {
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+      <h1>Solutions</h1>
+    </div>
+  )
+}
+export default Solutions
